9b815a9e39036875409631087158bdc6ca5e68dc
Maksym <qwerty@users.noreply.github.com>
2019-06-01 09:43:28 +0200

    final collections
    Co-Authored-By: Maksym <ehotqwerty@users.noreply.github.com>

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -18,12 +18,32 @@ namespace Collections
             objectList.Add("bye");
             ListPrint(objectList);
 
+            objectList.RemoveAt(0);
+            objectList.RemoveAt(0);
+            ListPrint(objectList);
+
+            Console.WriteLine($"Total number of items in the collection: {objectList.Count}");
+            List<string> countries = new List<string> { "Russia", "USA", "Ukraine", "Poland", "Japan" };
+            countries.Add("France");
+            countries.Remove("Japan");
+            ListPrint(countries);
+
             Console.ReadKey();
         }
 
         static void ListPrint(ArrayList list)
         {
             foreach(object o in list){
+                Console.Write(o);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+
+        static void ListPrint(List<string> list)
+        {
+            foreach (object o in list)
+            {
                 Console.Write(o);
                 Console.Write(' ');
             }
