4f91107e1063af4bb0ffdfdf2dccbadf04990fe7
Maksym <qwerty@users.noreply.github.com>
2019-06-01 09:24:29 +0200

    Collections v1

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Collections
 {
@@ -10,7 +11,23 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            ArrayList objectList = new ArrayList() { 0, 42, "hello", 's', true, 2.0f };
+            object obj = 68.2;
 
+            objectList.Add(obj);
+            objectList.Add("bye");
+            ListPrint(objectList);
+
+            Console.ReadKey();
+        }
+
+        static void ListPrint(ArrayList list)
+        {
+            foreach(object o in list){
+                Console.Write(o);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
         }
     }
 }
